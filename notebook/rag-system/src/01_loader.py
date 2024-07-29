3488b2c554f3071c5cd5e1d101e3e1470d7f6bf8
Ilya Suchkov <makeeoutchill@gmail.com>
2024-07-29 17:43:49 +0300

    Added some changes

--- notebook/rag-system/src/01_loader.py
+++ notebook/rag-system/src/01_loader.py
@@ -1,7 +1,6 @@
 from llama_index.core import SimpleDirectoryReader
 from llama_index.core import download_loader
-from llama_index.core import Document
-from llama_index.readers.database import DatabaseReader
+# from llama_index.readers.database import DatabaseReader
 from llama_index.core import VectorStoreIndex
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core import SimpleDirectoryReader
@@ -12,41 +11,45 @@ import os
 
 documents = SimpleDirectoryReader("./data").load_data()
 
-reader = DatabaseReader(
-    scheme=os.getenv("DB_SCHEME"),
-    host=os.getenv("DB_HOST"),
-    port=os.getenv("DB_PORT"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASS"),
-    dbname=os.getenv("DB_NAME"),
-)
+# reader = DatabaseReader(
+#     scheme=os.getenv("DB_SCHEME"),
+#     host=os.getenv("DB_HOST"),
+#     port=os.getenv("DB_PORT"),
+#     user=os.getenv("DB_USER"),
+#     password=os.getenv("DB_PASS"),
+#     dbname=os.getenv("DB_NAME"),
+# )
+# query = "SELECT * FROM users"
+# documents = reader.load_data(query=query)
 
-query = "SELECT * FROM users"
-documents = reader.load_data(query=query)
 
+# to create document instance manually 
+from llama_index.core import Document
 doc = Document(text="text")
 
-#High Level
-
+# HIGH LEVEL
+# for controlling document split-up more precise
+# creates Nodes which store text, metadata (as Document) and the relationship to their parent
 vector_index = VectorStoreIndex.from_documents(documents)
 vector_index.as_query_engine()
 
+# customizing core components (pass custom transformations list or apply global Settings)
 text_splitter = SentenceSplitter(chunk_size=512, chunk_overlap=10)
-
-
 Settings.text_splitter = text_splitter
-# per-index
-index = VectorStoreIndex.from_documents(
-    documents, transformations=[text_splitter]
-)
+index = VectorStoreIndex.from_documents(documents, transformations=[text_splitter]) # per-index
 
-#Low Level
-
+# LOW LEVEL
+# key step to process your documents is to split them into "chunks"/Node objects.
+# The key idea is to process your data into bite-sized pieces that can be retrieved / fed to the LLM.
 documents = SimpleDirectoryReader("./data").load_data()
 pipeline = IngestionPipeline(transformations=[TokenTextSplitter(), ...])
 nodes = pipeline.run(documents=documents)
 
-document = Document(
-    text="text",
-    metadata={"filename": "<doc_file_name>", "category": "<category>"},
-)
+# if needed add metadata for documents
+document = Document(text="text", metadata={"filename": "<doc_file_name>", "category": "<category>"})
+
+# passing Nodes directly into vector index (indexer)
+from llama_index.core.schema import TextNode
+node1 = TextNode(text="<text_chunk>", id_="<node_id>")
+node2 = TextNode(text="<text_chunk>", id_="<node_id>")
+index = VectorStoreIndex([node1, node2])
--- notebook/rag-system/src/02_index.py
+++ notebook/rag-system/src/02_index.py
@@ -1,3 +1,5 @@
 from llama_index.core import VectorStoreIndex
 
+# to create an index vectors from Nodes or Documents
 index = VectorStoreIndex.from_documents(documents)
+index = VectorStoreIndex(nodes)
